89f6a8d492da173ab08a8a69f33893974e96cf49
Eagan Martin <mece.martinece@gmail.com>
2018-12-10 13:00:03 +0800

    Moved main activity to activites sub package; Added function to check if database is open;

--- app/src/main/java/com/fujiyama/pulp/sqlitedb/activities/MainActivity.java
+++ app/src/main/java/com/fujiyama/pulp/sqlitedb/activities/MainActivity.java
@@ -1,9 +1,10 @@
-package com.fujiyama.pulp.sqlitedb;
+package com.fujiyama.pulp.sqlitedb.activities;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.fujiyama.pulp.sqlitedb.R;
 import com.fujiyama.pulp.sqlitedb.configs.sqlite.DatabaseHelper;
 import com.fujiyama.pulp.sqlitedb.configs.sqlite.DatabaseManager;
 
