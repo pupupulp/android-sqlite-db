dfc9cda3395620e4ac5803ef5e7649cc8d4ff1de
Eagan Martin <mece.martinece@gmail.com>
2018-12-10 16:59:06 +0800

    Revised database info variable from private to public;

--- app/src/main/java/com/fujiyama/pulp/sqlitedb/configs/sqlite/schema/DatabaseInfo.java
+++ app/src/main/java/com/fujiyama/pulp/sqlitedb/configs/sqlite/schema/DatabaseInfo.java
@@ -1,14 +1,7 @@
 package com.fujiyama.pulp.sqlitedb.configs.sqlite.schema;
 
 public class DatabaseInfo {
-    private static final String DATABASE_NAME = "sample.db";
-    private static final int DATABASE_VERSION = 1;
 
-    public static String getDatabaseName() {
-        return DATABASE_NAME;
-    }
-
-    public static int getDatabaseVersion() {
-        return DATABASE_VERSION;
-    }
+    public static final String DATABASE_NAME = "sample.db";
+    public static final int DATABASE_VERSION = 1;
 }
