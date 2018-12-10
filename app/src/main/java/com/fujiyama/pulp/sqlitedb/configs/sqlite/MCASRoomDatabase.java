c29a314f3f72d8c87c57e9bf76da0c420bdfbce5
Eagan Martin <mece.martinece@gmail.com>
2018-12-10 17:06:13 +0800

    Added handling of instance of room database;

--- app/src/main/java/com/fujiyama/pulp/sqlitedb/configs/sqlite/MCASRoomDatabase.java
+++ app/src/main/java/com/fujiyama/pulp/sqlitedb/configs/sqlite/MCASRoomDatabase.java
@@ -1,7 +1,9 @@
 package com.fujiyama.pulp.sqlitedb.configs.sqlite;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 import com.fujiyama.pulp.sqlitedb.configs.sqlite.dao.UserDAO;
 import com.fujiyama.pulp.sqlitedb.configs.sqlite.schema.DatabaseInfo;
@@ -13,4 +15,20 @@ import com.fujiyama.pulp.sqlitedb.configs.sqlite.schema.tables.User;
 public abstract class MCASRoomDatabase extends RoomDatabase {
 
     public abstract UserDAO userDAO();
+
+    private static volatile MCASRoomDatabase instance;
+
+    static MCASRoomDatabase getInstance(final Context context) {
+        if(instance == null) {
+            synchronized (MCASRoomDatabase.class) {
+                if(instance == null) {
+                    instance = Room.databaseBuilder(context.getApplicationContext(),
+                            MCASRoomDatabase.class,
+                            DatabaseInfo.DATABASE_NAME).build();
+                }
+            }
+        }
+
+        return instance;
+    }
 }
