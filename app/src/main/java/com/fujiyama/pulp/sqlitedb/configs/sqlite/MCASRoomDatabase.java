6ed43b9c798ec9d4d3c7238e228ab0491993919a
Eagan Martin <mece.martinece@gmail.com>
2018-12-10 18:20:20 +0800

    Revised data type of id in user; Added callback on open of database;

--- app/src/main/java/com/fujiyama/pulp/sqlitedb/configs/sqlite/MCASRoomDatabase.java
+++ app/src/main/java/com/fujiyama/pulp/sqlitedb/configs/sqlite/MCASRoomDatabase.java
@@ -1,13 +1,18 @@
 package com.fujiyama.pulp.sqlitedb.configs.sqlite;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 import com.fujiyama.pulp.sqlitedb.configs.sqlite.dao.UserDAO;
 import com.fujiyama.pulp.sqlitedb.configs.sqlite.schema.DatabaseInfo;
 import com.fujiyama.pulp.sqlitedb.configs.sqlite.schema.tables.User;
+
+import java.math.BigInteger;
 
 @Database(entities = {
         User.class
@@ -24,11 +29,39 @@ public abstract class MCASRoomDatabase extends RoomDatabase {
                 if(instance == null) {
                     instance = Room.databaseBuilder(context.getApplicationContext(),
                             MCASRoomDatabase.class,
-                            DatabaseInfo.DATABASE_NAME).build();
+                            DatabaseInfo.DATABASE_NAME)
+                            .addCallback(populateDatabase)
+                            .build();
                 }
             }
         }
 
         return instance;
+    }
+
+    private static RoomDatabase.Callback populateDatabase = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDbAsync(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+        private final UserDAO mDao;
+
+        PopulateDbAsync(MCASRoomDatabase database) {
+            mDao = database.userDAO();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... voids) {
+            mDao.deleteAll();
+
+            User user = new User(5, "John Doe");
+            mDao.insert(user);
+
+            return null;
+        }
     }
 }
--- app/src/main/java/com/fujiyama/pulp/sqlitedb/configs/sqlite/schema/tables/User.java
+++ app/src/main/java/com/fujiyama/pulp/sqlitedb/configs/sqlite/schema/tables/User.java
@@ -5,25 +5,23 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
-import java.math.BigInteger;
-
 @Entity(tableName = "USERS")
 public class User {
 
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "ID")
     @NonNull
-    private BigInteger mId;
+    private int mId;
 
     @ColumnInfo(name = "FULL_NAME")
     private String mFullName;
 
-    public User(BigInteger mId, String mFullName) {
+    public User(int mId, String mFullName) {
         this.mId = mId;
         this.mFullName = mFullName;
     }
 
-    public BigInteger getmId() {
+    public int getmId() {
         return mId;
     }
 
